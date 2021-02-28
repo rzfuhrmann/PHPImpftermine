a24655952fafa20e55694584d406ffa622c17d9d
Sebastian Fuhrmann <sebastian.fuhrmann@rz-fuhrmann.de>
2021-02-28 17:04:03 +0100

    add caching

--- impftermine.class.php
+++ impftermine.class.php
@@ -7,24 +7,55 @@
      * @copyright   (C) 2020-2021 Rechenzentrum Fuhrmann Inh. Sebastian Fuhrmann
      * 
      * Roadmap:
-     * - add caching
+     * - add/improve caching
+     * - clean-up code
+     * - add examples and helper functions for looping through vaccination centers and vaccines
+     * - add logging
      */
     class Impftermine {
+        private $cachingDir = __DIR__.'/cache/';
+
         public function __construct(){
             
         }
 
-        private function doRequest($url){
-            $ch = curl_init(); 
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-            $raw = curl_exec($ch); 
-            $info = curl_getinfo($ch);
-            curl_close($ch); 
-
-            if ($info["http_code"] == 200 && ($json = json_decode($raw, true))){
-                return $json; 
+        public function setCachingDir ($path){
+            if (file_exists($path)){
+                $this->cachingDir = $path.'/'; 
             }
+        }
+
+        private function isCachingEnabled(){
+            return !!file_exists($this->cachingDir); 
+        }
+
+        private function doRequest($url, $cfg = array("cachingTime" => 1)){
+            $res = null; 
+
+            // write cache file, but don't re-use it long
+            if (!isset($cfg["cachingTime"])) $cfg["cachingTime"] = 1; 
+
+            $cache_fn = $this->cachingDir.md5($url).".json";
+            if ($this->isCachingEnabled() && file_exists($cache_fn) && time() < filemtime($cache_fn)+$cfg["cachingTime"]){
+                // @todo: Error handling
+                $res = json_decode(file_get_contents($cache_fn), true); 
+            } else {
+                $ch = curl_init(); 
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+                $raw = curl_exec($ch); 
+                $info = curl_getinfo($ch);
+                curl_close($ch); 
+
+                if ($info["http_code"] == 200 && ($json = json_decode($raw, true))){
+                    $res = $json; 
+                    if ($this->isCachingEnabled()){
+                        file_put_contents($cache_fn, json_encode($json, JSON_PRETTY_PRINT));
+                    }
+                }
+            }
+            
+            return $res; 
         }
 
         /**
@@ -33,7 +64,12 @@
          * Endpoint: https://www.impfterminservice.de/assets/static/its/vaccination-list.json
          */
         public function getVaccines (){
-            $list = $this->doRequest('https://www.impfterminservice.de/assets/static/its/vaccination-list.json');
+            $list = $this->doRequest(
+                        'https://www.impfterminservice.de/assets/static/its/vaccination-list.json', 
+                        array(
+                            "cachingTime" => 60*60*24     // will not change that often ;)
+                        )
+                    );
             return $list;
         }
 
@@ -43,7 +79,12 @@
          * Endpoint: https://www.impfterminservice.de/assets/static/impfzentren.json
          */
         public function getVaccinationCenters(){
-            $list = $this->doRequest('https://www.impfterminservice.de/assets/static/impfzentren.json');
+            $list = $this->doRequest(
+                        'https://www.impfterminservice.de/assets/static/impfzentren.json',
+                        array(
+                            "cachingTime" => 60*60*24     // will not change that often ;)
+                        )
+                    );
             return $list; 
         }
     }
